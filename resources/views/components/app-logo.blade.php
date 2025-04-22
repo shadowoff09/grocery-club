c8810cfe2c9e1dc720e7549809a6accd22895f22
shadowoff09 <diogogaspar931@gmail.com>
2025-04-22 16:16:30 +0100

    feat: update application name and logo, add blade-lucide-icons package

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+    <x-lucide-shopping-cart class="w-6 h-6" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-none font-semibold">Grocery Club</span>
 </div>
